d0ced2b3d82917fd0bb02c710608c52cbcd457a2
Jeet7781-mahi <risavsinha84@gmail.com>
2025-10-15 21:12:54 +0530

    First Project
    Create project on upperCase

--- initials/src/components/TextForm.js
+++ initials/src/components/TextForm.js
@@ -1,18 +1,29 @@
 import React,{useState} from 'react'
 
-const[text,setText]=useState("Enter your text");
+
 export default function TextForm(params) {
+    const[text,setText]=useState("Enter your text");
+    const handleUpClick = ()=>{
+        console.log("Clicked the button")
+        let newText=text.toUpperCase()
+        setText(newText)
+    }
+    const handleOnChange = (event) =>{
+        console.log("Now the text-area is writtable")
+        setText(event.target.value)
+    }
+
     return (
         <div>
             <form style={{background: params.background}}>
                 <div className="mb-3">
                     <h1>{params.heading}</h1>
                     <label htmlFor="myBox" className="form-label">See the difference</label>
-                    <textarea type="email" rows={8} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <textarea type="email" rows={8} value={text} onChange={handleOnChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     <div id="myBox" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                
-                <button type="submit" className="btn btn-primary">Convert</button>
+                <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
             </form>
         </div>
     )
