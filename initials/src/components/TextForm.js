c246fc2c92862478e68f75330811477e973b08db
Jeet7781-mahi <risavsinha84@gmail.com>
2025-11-20 20:39:45 +0530

    Update textform
    State practice

--- initials/src/components/TextForm.js
+++ initials/src/components/TextForm.js
@@ -3,14 +3,34 @@ import React,{useState} from 'react'
 
 export default function TextForm(params) {
     const[text,setText]=useState("Enter your text");
+    const[background,setBackground]=useState("white");
+    const bgColor =()=>{
+        //let cs=document.getElementById("root").css(background,'darkcyan')
+        setBackground("darkcyan")
+    }
     const handleUpClick = ()=>{
         console.log("Clicked the button")
         let newText=text.toUpperCase()
         setText(newText)
     }
+    const handleDownClick=()=>{
+        const lowKey=text.toLowerCase()
+        setText(lowKey)
+    }
+    const clearOnClick=()=>{
+        const clearText=""
+        setText(clearText)
+    }
+    const copyClipBoard = () =>{
+        const newText1 =navigator.clipboard.writeText(text)
+        setText(newText1)
+    }
     const handleOnChange = (event) =>{
         console.log("Now the text-area is writtable")
+       
         setText(event.target.value)
+        
+
     }
 
     return (
@@ -21,9 +41,14 @@ export default function TextForm(params) {
                     <h1>{params.heading}</h1>
                     <label htmlFor="myBox" className="form-label">See the difference</label>
                     <textarea type="email" rows={8} value={text} onChange={handleOnChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <p>{200-text.length}/{200}</p>
                     <div id="myBox" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={handleDownClick}>Convert to LowerCase</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={clearOnClick}>Clear Text</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={copyClipBoard}>Copy Text</button>
+                <button type="button" className="btn btn-primary mx-3" onClick={bgColor}>Change Background</button>
             </form>
         </div>
         <div className="container">
